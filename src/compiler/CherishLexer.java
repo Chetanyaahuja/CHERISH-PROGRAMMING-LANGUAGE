91d9eb8ec672bebaae55311b43cac79d2c26e861
Chetanyaahuja <35379403+Chetanyaahuja@users.noreply.github.com>
2018-04-28 04:42:26 -0700

    Updated Lexer according to new Grammar

--- src/compiler/CherishLexer.java
+++ src/compiler/CherishLexer.java
@@ -1,4 +1,5 @@
-package src.compiler;// Generated from ../grammar/Cherish.g4 by ANTLR 4.7.1
+package src.compiler;
+// Generated from ../grammar/Cherish.g4 by ANTLR 4.7.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
