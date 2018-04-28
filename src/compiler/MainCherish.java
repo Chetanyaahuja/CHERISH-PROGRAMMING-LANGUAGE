11e482331be1bdd6b7d4f52582bb6deecaeaddd5
Drishty Kapoor <35379752+Drishtykapoor@users.noreply.github.com>
2018-04-28 04:52:17 -0700

    Updated MainCherish.java

--- src/compiler/MainCherish.java
+++ src/compiler/MainCherish.java
@@ -5,12 +5,15 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 public class MainCherish {
 
         public static void main(String args[]){
-            String filename = "/Users/natalyakumar/Documents/Workspace/ser516/SER502-Spring2018-Team31/grammar/Example.txt";
+            String filename = "grammar/Example.txt";
             try {
                 CherishLexer lexer = new CherishLexer(CharStreams.fromFileName(filename));
 
@@ -22,10 +25,24 @@ public class MainCherish {
 
                 System.out.println(parseTree.toStringTree(parser));
 
-                CherishCompiler compiler = new CherishCompiler();
-                compiler.visit(parseTree);
-
-                System.out.println(compiler.getICOutput());
+                CherishIC ic = new CherishIC();
+                ParseTreeWalker.DEFAULT.walk(ic, parseTree);
+               /* CherishCompiler compiler = new CherishCompiler();
+                compiler.visit(parseTree);*/
+                
+                StringBuilder icCode = ic.getICOutput();
+                System.out.println(icCode);
+                try {
+        				PrintWriter writer = new PrintWriter(filename.substring(0, filename.length()-3)+"cherish", "UTF-8");
+        				writer.println(icCode.toString());
+        				writer.close();
+	        		} catch (FileNotFoundException e) {
+	        			// TODO Auto-generated catch block
+	        			e.printStackTrace();
+	        		} catch (UnsupportedEncodingException e) {
+	        			// TODO Auto-generated catch block
+	        			e.printStackTrace();
+	        		}
 
             } catch (IOException e) {
                 e.printStackTrace();
